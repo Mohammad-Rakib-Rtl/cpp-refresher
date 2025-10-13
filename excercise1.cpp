f54e82efc9c1065daeaebb16d3a40cbc3ac221e7
Mohammad-Rakib-Rtl <rtzrakib347@gmail.com>
2025-10-14 00:57:18 +0800

    latest commit

---
+++ excercise1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by roman on 10/14/2025.
+//
+#include <iostream>
+using namespace std;
+int main() {
+
+    double x = 10;
+    double y= 5;
+
+    double z = (x + 10) / (3 * y);
+
+    cout << z << endl;
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main(){
 //    int file_size = 100, counter = 0;
-    int file_size = 100;
+//    int file_size = 100;
 //    int counter = 0;
     double sales = 9.99;
 
@@ -14,10 +14,47 @@ int main(){
 
 //    pi = 3.12;
 
+//Naming conventions
+
+    int file_size; //Snake Case
+    int FileSize; // Pascal Case //will use for classes
+    int fileSize; //Camel Case //in this tutorial author will use this for declaring variables and cosnstants
+    int iFileSize; //Hungarian Notation
+
+    //Mathematical Expressions
+    double x = 10;
+    int y = 3;
+    double z = x / y;
+
+    std::cout << z << endl;
+
+    int w = 10;
+    int x1 = w++;
+//    w = w + 1;
+//    w++;
+//    ++w;
+//    int y1 = ++w;
+
+    cout << "-----------------" << endl;
+    cout << w << "  " << x1 << endl;
+//    cout << w << "  " << y1 << endl;
+    cout << "-----------------" << endl;
 
     std::cout << "Hello World" << std::endl;
     std::cout << file_size << std::endl;
-    std::cout << sales;
+    std::cout << sales << endl;
+
+    cout << "-------Order/Priorities of operators----------" << endl;
+    // () //we can change the order by using parenthesis
+    double zx = (1 + 2) * 3;
+    // * and /
+    // + and -
+    double xz = 1 + 2 * 3;
+
+    cout << xz << endl;
+    cout << zx << endl;
+
+
     return 0;
 
 }
